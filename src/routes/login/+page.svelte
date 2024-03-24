6f8c07f0b4afb7362372e5131e56cfbda9a3a0f9
laxitshahi <shahilax@my.yorku.ca>
2024-03-23 20:04:03 -0400

    added login css

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,22 @@
-<h1>Login Page</h1>
+<div class="flex justify-end h-screen">
+	<!-- Backdrop -->
+	<div class="bg-gray-200 flex-grow h-screen" />
+	<section
+		class="rounded-2xl border-2 w-2/5 justify-center grid place-items-center shadow-lg h-screen"
+	>
+		<div>
+			<h2 class="text-3xl">Welcome Back!</h2>
+			<div class="my-20" />
+			<div class="grid">
+				<a class="flex justify-center bg-gray-100 px-4 py-2 rounded-3xl" href="/auth/login/github"
+					>Login in with Github</a
+				>
 
-<a href="/auth/login/github">Login in with Github</a>
-<a href="/auth/login/google">Login in with Google</a>
+				<div class="my-2" />
+				<a class="flex justify-center bg-gray-100 px-4 py-2 rounded-3xl" href="/auth/login/google"
+					>Login in with Google</a
+				>
+			</div>
+		</div>
+	</section>
+</div>
